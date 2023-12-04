b04cea4a97f4252096fd75537e31c6aacc3b5461
Rudram1 <138531180+Rudram1@users.noreply.github.com>
2023-12-04 22:01:34 +0000

    Conversion

--- basics/test.js
+++ basics/test.js
@@ -1,8 +1,14 @@
-console.log("Harihar");
-console.table([])
+//console.log("Harihar");
+//console.table([])
 //primitive data types
 //symbol => unique
 
 //Non-primitive datatypes
 //object
 /*console.log(typeof null);object*/ 
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
