2f97a4ffbd1af32a7f1570f13325cda09b5e737b
Rudram1 <138531180+Rudram1@users.noreply.github.com>
2023-12-07 02:33:46 +0000

    conversion

--- basics/test.js
+++ basics/test.js
@@ -12,3 +12,6 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+//Comaprison convert null to number, treating it as 0.
+//avoid null and undefined comparison.
+
