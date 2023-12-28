84184f85e1612b8d135836d7a146b8ae69b8baa6
Rudram1 <138531180+Rudram1@users.noreply.github.com>
2023-12-28 21:27:23 +0000

    Save

--- basics/test.js
+++ basics/test.js
@@ -15,3 +15,41 @@ console.log(typeof stringNumber);
 //Comaprison convert null to number, treating it as 0.
 //avoid null and undefined comparison.
 
+//primitive: string, number, boolean,null,undefined,symbol,bigint
+
+//non-primitive or refernece: Arrays,objects,functions.
+const heros= ["shaktimaan","naagraj","raga"];
+let myObj ={
+    name: "suraj",
+    age:32,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+//stack(primimtive), heap(non-primitive)
+
+//strings
+const name ="hitesh";
+const repoCount = 60;
+
+//console.log(name + repoCount + " Value"); outdated/
+
+console.log(`Hello My name is ${name} and my repo count is ${repoCount}.`);//template literals
+
+const gameName = new String('Surajidk');
+console.log(gameName.__proto__);
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('j'));
+
+const newString = gameName.substring(1,5);
+console.log(newString);
+
+
+
+
+
+
+
