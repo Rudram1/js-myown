8d28d21cd2fa68cbbea3472b297e69f1237b4775
Rudram1 <138531180+Rudram1@users.noreply.github.com>
2023-12-28 21:53:28 +0000

    Math

---
+++ basics/number.js
@@ -0,0 +1,23 @@
+const balance = new Number(100);
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber = 123.8967;
+
+console.log(otherNumber.toPrecision(4));
+
+const limit = 100000000
+console.log(limit.toLocaleString('en-IN'));
+
+
+//++++++++++++++Math+++++++++++++++++++++
+//console.log(Math.round(4.5));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 5;
+const max = 7;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
