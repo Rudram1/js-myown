e6902ad7082cca13da7f34d9093774c8ecc30257
Rudram1 <138531180+Rudram1@users.noreply.github.com>
2024-01-19 00:21:12 +0000

    api

--- basics/object.js
+++ basics/object.js
@@ -6,4 +6,22 @@ tinderUser.id = "123abc"
 tinderUser.name = "Aman"
 tinderUser.isloggin = false
 
-console.log(tinderUser);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isloggin'));
+
+const course = {
+    coursename : "my buiding",
+    price: "899",
+    courseInstructor: "GOD"
+}
+
+
+//course.courseInstructor
+
+const {price} = course
+
+console.log(price);
